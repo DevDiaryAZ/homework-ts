9cb014af4fc190e3661b1813ba68fd0b01d76f3c
Alenka <alenazarubi@gmail.com>
2022-07-20 00:56:33 +0300

    fix bug in Message.tsx and added styles in Message.module.css

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import styles from "./Message.module.css"
 
-function Message() {
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
     return (
-        <div>
-
+        <div className={styles.message}>
+            <div className={styles.message__imageContainer}>
+                <img src={props.avatar} alt=""/>
+            </div>
+            <div className={styles.message__textContainer}>
+                <div className={styles.message__name}>{props.name}</div>
+                <div className={styles.message__text}>{props.message}</div>
+                <div className={styles.message__time}>
+                    {props.time}
+                </div>
+                <div className=
+                         {styles.message__leftTriangle}></div>
+            </div>
         </div>
     )
 }
